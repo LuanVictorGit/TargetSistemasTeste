d07cd62848c5b0f1c497f8e3ac94de7fc4d20359
LuanVictorGit <luanvictorchagas2015@gmail.com>
2024-09-15 14:11:30 -0300

    first commit

---
+++ src/me/targetteste/Faturamento.java
@@ -0,0 +1,13 @@
+package me.targetteste;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Faturamento {
+
+	private int dia;
+    private double valor;
+	
+}
